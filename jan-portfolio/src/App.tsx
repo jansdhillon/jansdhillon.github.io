769bcbea8b2a84e5cbbb8ae76dd2b470b46be40e
jansdhillon <77344313+jansdhillon@users.noreply.github.com>
2023-02-19 21:21:49 -0800

    experimenting with react-bootstrap

--- jan-portfolio/src/App.tsx
+++ jan-portfolio/src/App.tsx
@@ -1,43 +1,39 @@
-import { useState } from 'react'
-import './App.css';
-import Sound, {ReactSoundProps} from 'react-sound';
-import LayerCake from './assets/bg.mp3';
+import { useState, useRef, useEffect } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import autoAnimate from "@formkit/auto-animate";
+import './App.css'
+import Sound, {ReactSoundProps} from 'react-sound'
+import LayerCake from '/bg.mp3'
+import { Button, Stack } from 'react-bootstrap';
 
 function App() {
-  const [status, setStatus] = useState<ReactSoundProps['playStatus']>('STOPPED');
-
-  function togglePlayStatus() {
-    setStatus(status => status === 'STOPPED' ? 'PLAYING' : 'STOPPED')
-  }
-
-  function statusLabel(status: ReactSoundProps['playStatus']): string {
-    switch(status) {
-      case 'STOPPED':
-        return 'PLAY';
-      case 'PLAYING':
-        return 'STOP';
-      default:
-        return 'STOP';
-    }
-  }
+  const [status, setStatus] = useState<ReactSoundProps['playStatus']>('PLAYING');
 
   return (
-    <div className="App">
-      <h1 className="title">Jan-Yaeger Dhillon</h1>
-      <div>
-        <a href="https://github.com/jansdhillon" className="github">GitHub</a>
-        <a href="https://www.linkedin.com/in/jan-yaeger-dhillon-572674239/" className="linkedin">LinkedIn</a>
+      <div className="App">
+      <div className="header">
+        <h1>Jan Yaeger Dhillon</h1>
       </div>
-      <div className="card">
+      <Button as="a" variant="success" href="https://www.linkedin.com/in/jan-yaeger-dhillon-572674239/">
+        GitHub
+      </Button>
+      <Button as="a" variant="primary" href="https://github.com/jansdhillon">
+        LinkedIn
+      </Button>
+      <h2 className="center">Hey, I'm Jan. Welcome to my personal page! I'll be updating this regularily as my web development skills advance.</h2><div className="card">
         <h2>About Me</h2>
-        <p className="center">Hey, I'm Jan. Welcome to my personal page! I'll be updating this regularily as my web develoopment skills advance.</p>
+        <p className="center">I'm a fourth year at UBC Okanagan pursuing a degree in computer science. I like to learn new things and love
+          to use coding as an outlet for creativity. I've really been enjoying Python but I'm also trying to learn web development (like this!)</p>
+      </div><div className="card">
+        <h2>Projects</h2>
+        <p className="center">This portfolio is the main project I'm working on right now, but you can find more things I've done over at my GitHub!</p>
       </div>
-      <button onClick={(click) => togglePlayStatus()}>{statusLabel(status)}</button>
       <Sound
         url={LayerCake}
-        playStatus={status}
+        playStatus={"PLAYING"}
       />
     </div>
+  
   );
 }
 
