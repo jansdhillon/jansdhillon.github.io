9cd6670f00e0caa9499f65bd3a96e53a89e369f3
jansdhillon <77344313+jansdhillon@users.noreply.github.com>
2023-03-03 12:17:36 -0800

    trying to use AOS

---
+++ jan-portfolio/src/Animation.tsx
@@ -0,0 +1,11 @@
+import ScrollAnimation from ‘react-animate-on-scroll’
+
+const Animation = () => (
+
+<ScrollAnimation animateBounce=”bounceIn”>
+
+<p>Text bounces in on scroll</p>
+
+</ScrollAnimation>
+
+)
