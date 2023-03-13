400fb7ff0efe9ed3b56e8c3f77aca7a9502b054b
jansdhillon <77344313+jansdhillon@users.noreply.github.com>
2023-03-12 22:47:18 -0700

    managed to get it working with github pages

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import HelloMessage from "./HelloMessage";
 import "animate.css/animate.min.css";
 import AOS from "aos";
 import React from "react";
