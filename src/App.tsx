3b4ded3b9145e4da3415bdb9b353f856f5938a12
jansdhillon <77344313+jansdhillon@users.noreply.github.com>
2023-03-18 20:11:43 -0700

    added skill icons and displays them rather than aa description.

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,29 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import autoAnimate from "@formkit/auto-animate";
 import "./App.css";
 import Sound, { ReactSoundProps } from "react-sound";
-import { Button, Stack } from "react-bootstrap";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import {
+  Button,
+  Stack,
+  Container,
+  Nav,
+  Navbar,
+  NavDropdown,
+  Card,
+} from "react-bootstrap";
 import "animate.css/animate.min.css";
 import AOS from "aos";
 import React from "react";
 import Writer from "./Writer";
 import github from "./assets/github.png";
 import linkedin from "./assets/linkedin.png";
+import python from "./assets/python.png";
+import java from "./assets/java.png";
+import javascript from "./assets/js.png";
+import react from "./assets/react.png";
+import html from "./assets/html.png";
+import css from "./assets/css.png";
+import ts from "./assets/ts.png";
+import sql from "./assets/sql.png";
 
 
 function App() {
@@ -23,9 +35,44 @@ function App() {
     });
   }, []);
 
+
+  const skills = [
+    {
+      name: "Python",
+      icon: python,
+    },
+    {
+      name: "Java",
+      icon: java,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "HTML",
+      icon: html,
+    },
+    {
+      name: "CSS",
+      icon: css,
+    },
+    {
+      name: "React",
+      icon: react,
+    },
+    {
+      name: "TypeScript",
+      icon: ts,
+    },
+    {
+      name: "SQL",
+      icon: sql,
+    }
+  ];
+
   return (
     <div className="App">
-      {/*Find a way to make the Navbar solid*/}
       <Navbar expand="lg" fixed="top">
         <Container>
           <Navbar.Brand className="tab" href="#intro">
@@ -48,14 +95,24 @@ function App() {
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
-          {/*TODO: Change buttons to logo icons*/}
           <span>
             <a href="https://www.github.com/jansdhillon">
-            <img
-              src={github} className="icon" alt="GitHub" width="50" height="50"/>
+              <img
+                src={github}
+                className="icon"
+                alt="GitHub"
+                width="50"
+                height="50"
+              />
             </a>
             <a href="https://www.linkedin.com/in/jan-yaeger-dhillon-572674239/">
-            <img src={linkedin} className="icon" alt="LinkedIn" width="50" height="50"/>
+              <img
+                src={linkedin}
+                className="icon"
+                alt="LinkedIn"
+                width="50"
+                height="50"
+              />
             </a>
           </span>
         </Container>
@@ -77,16 +134,29 @@ function App() {
         </p>
       </div>
       <Container id="skills" className="section">
-        <h2
-          data-aos="fade-down"
-          data-aos-duration="700"
-          data-aos-anchor="#skills"
-        >
-          Skills
-        </h2>
-        <p className="center" data-aos="fade-right" data-aos-duration="1000">
-          I have experience programming with Python, JAVA, and JavaScript, and I'm currently learning more about web development technologies like React.
-        </p>
+          <h2
+            data-aos="fade-down"
+            data-aos-duration="700"
+            data-aos-anchor="#skills"
+          >
+            Skills
+          </h2>
+          <div className="d-flex flex-wrap justify-content-center">
+            {skills.map((skill) => (
+              <div
+                className="skill-card"
+                data-aos="zoom-in"
+                data-aos-duration="1000"
+              >
+                <img
+                  src={skill.icon}
+                  alt={skill.name}
+                  style={{ width: "50%", paddingBottom: "10px" }}
+                />
+                <h5>{skill.name}</h5>
+              </div>
+            ))}
+          </div>
       </Container>
       <Container id="projects" className="section">
         <h2
@@ -96,19 +166,24 @@ function App() {
         >
           Projects
         </h2>
-        <p className="center" data-aos="fade-right" data-aos-duration="1000">
-          This portfolio is the main project I'm working on right now, but you
-          can find more things I've done over at my GitHub!
-        </p>
+        <Card className="center" data-aos="fade-right" data-aos-duration="1000">
+          <Card.Body>
+            This portfolio is the main project I'm working on right now, but you
+            can find more things I've done over at my GitHub!
+          </Card.Body>
+        </Card>
       </Container>
       <Container id="ee" className="section">
         <h2 data-aos="fade-down" data-aos-duration="700" data-aos-anchor="#ee">
           Education and Experience
         </h2>
-        <p className="center" data-aos="fade-right" data-aos-duration="1000">
-          I'm currently in my fourth-year of my computer science degree at UBC
-          Okanagan. I also work as the Student Support Analyst for UBCIT.
-        </p>
+        <Card className="center" data-aos="fade-right" data-aos-duration="1000">
+          <Card.Body>
+            I'm currently in my fourth-year of my computer science degree at
+            UBC Okanagan. I also work as the Student Support Analyst for
+            UBCIT.
+          </Card.Body>
+        </Card>
       </Container>
     </div>
   );
