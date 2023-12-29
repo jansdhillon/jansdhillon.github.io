a9a0205bacc0d0f4871854e4c6329c4eb88154eb
jansdhillon <jandhilL@student.ubc.ca>
2023-12-28 22:00:28 -0700

    updates incl. insta, x, armilla, anchors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,28 +13,48 @@ import { Github } from "lucide-react";
 import { RxGithubLogo } from "react-icons/rx";
 import { useState } from "react";
 import { motion } from "framer-motion";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
 
 const IndexPage: NextPage = () => {
   const [selectedId, setSelectedId] = useState(null);
 
   return (
-    <motion.div
-      drag="x"
-      dragConstraints={{ left: 0}}
-      className="grid grid-cols-1 place-items-center place-content-center gap-4 lg:grid-cols-2 mx-auto  p-5 lg:h-screen "
-    >
+    <div className="grid grid-cols-1 place-items-center place-content-center gap-4 lg:grid-cols-2 mx-auto  p-5 lg:h-screen ">
       <div className="grid grid-rows-3 gap-4 h-full">
         <Card className=" rounded-xl grid items-center row-span-2 bg-card border-2  border-accent bg-opacity-30 drop-shadow-md">
-          <CardHeader>
+          <CardHeader className="flex flex-col">
             <CardTitle className="text-5xl font-bold bg-gradient-to-r from-orange-300  to-indigo-500 inline-block text-transparent bg-clip-text animate-gradient bg-300%">
               Jan-Yaeger Dhillon
             </CardTitle>
-            <CardDescription className="text-2xl font-bold py-5">
+            <CardDescription className="text-2xl font-semibold py-5">
               Welcome to my portfolio!
             </CardDescription>
+            <div className="grid grid-cols-3 gap-2 ">
+              <a href="https://www.github.com/jansdhillon">
+                <div className="flex flex-row gap-2 place-items-center">
+                  <RxGithubLogo className="h-5 w-5 font-semibold" />
+                  <p>jansdhillon</p>
+                </div>
+              </a>
+              <a href="https://www.x.com/JanDhillon">
+                <div className="flex flex-row gap-2 place-items-center">
+                  <FaXTwitter className="h-5 w-5 font-semibold" />{" "}
+                  <p>jandhillon</p>
+                </div>
+              </a>
+              <a href="https://www.instagram.com/jandhillon">
+                <div className="flex flex-row gap-2 place-items-center">
+                  <FaInstagram className="h-5 w-5 font-semibold" />{" "}
+                  <p>jandhillon</p>
+                </div>
+              </a>
+            </div>
           </CardHeader>
-          <CardContent className=" text-xl font-semibold">
-            ffffffffffffffffff
+
+          <CardContent className=" text-xl font-regular">
+            Hey, I'm Jan! I'm an aspiring software engineer who enjoys building
+            and learning new things.
           </CardContent>
         </Card>
         <Card className="bg-opacity-60 bg-card  border-accent border-2 drop-shadow-md ">
@@ -87,15 +107,15 @@ const IndexPage: NextPage = () => {
         </Card>
         <Card className="bg-opacity-60 bg-card  border-accent border-2 drop-shadow-md">
           <CardHeader className="grid grid-cols-2 w-full">
-            <CardTitle className="text-3xl">obscurus</CardTitle>
+            <CardTitle className="text-2xl">obscurus</CardTitle>
             <Image
               src="/logo.svg"
-              alt="Recycling"
+              alt="obscurus"
               width={50}
               height={50}
               className="justify-self-end"
             />
-            <CardDescription className="text-xl">
+            <CardDescription className="text-lg">
               Capstone Software Engineering Project
             </CardDescription>
           </CardHeader>
@@ -107,9 +127,17 @@ const IndexPage: NextPage = () => {
         </Card>
         <Card className="bg-opacity-60 bg-card  border-accent border-2 drop-shadow-md ">
           <CardHeader className="grid grid-cols-2 w-full">
-            <CardTitle className="text-2xl">Project</CardTitle>
-            <RxGithubLogo className="h-10 w-10 justify-self-end" />
-            <CardDescription></CardDescription>
+            <CardTitle className="text-2xl">Software Engineer Intern</CardTitle>
+            <Image
+              src="/at-yellow-cropped.svg"
+              alt="armilla"
+              width={40}
+              height={40}
+              className="justify-self-end"
+            />
+            <CardDescription className="text-lg">
+              Armilla Technology
+            </CardDescription>
           </CardHeader>
           <CardContent className="text-lg ">
             I&apos;m in my last year of my Computer Science degree at UBC
@@ -118,7 +146,7 @@ const IndexPage: NextPage = () => {
           </CardContent>
         </Card>
       </div>
-    </motion.div>
+    </div>
   );
 };
 
