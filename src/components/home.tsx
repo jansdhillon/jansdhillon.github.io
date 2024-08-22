cd7e6efe18d5d50cd29a6719bb9d38c6b8986add
jansdhillon <imightbejan@gmail.com>
2024-08-22 17:50:51 -0600

    make status badges col on mobile

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Badge } from "@/components/ui/badge";
-import  Writer  from "./writer";
+import Writer from "./writer";
 import { Socials } from "./socials";
 import { Footer } from "./footer";
 import { Projects } from "./projects";
@@ -14,7 +14,6 @@ import "aos/dist/aos.css";
 import { JobStatusBadge } from "./job-status-badge";
 
 export default function Home() {
-
   return (
     <div className="flex flex-col min-h-[100dvh] pt-24">
       <main className="flex-1">
@@ -22,14 +21,18 @@ export default function Home() {
           <div className="container px-6">
             <div className="flex flex-col items-center space-y-6 md:space-y-10 text-center">
               <div className="min-w-full font-bold min-h-[60px] bg-gradient-to-l from-indigo-400 via-fuchsia-300 to-orange-400 tracking-tighter text-transparent bg-clip-text animate-gradient bg-300%">
-                <Writer text="Hey, I'm Jan!" className="text-4xl md:text-6xl tracking-tighter" />
+                <Writer
+                  text="Hey, I'm Jan!"
+                  className="text-4xl md:text-6xl tracking-tighter"
+                />
               </div>
               <h2 className="text-md md:text-2xl font-medium">
                 Software Engineer
               </h2>
-              <div className="flex gap-3 items-center justify-center">
+              <div className="flex gap-3 flex-col md:flex-row  items-center justify-center">
                 <LocationBadge />
-                <JobStatusBadge/>
+
+                <JobStatusBadge />
               </div>
               <Socials />
             </div>
