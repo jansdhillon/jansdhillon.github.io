dfd371b745fed05403cfb5ec917e5b1b4703499f
jansdhillon <imightbejan@gmail.com>
2024-09-07 16:29:34 -0600

    fix screen sizing issue on mobile

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -10,8 +10,8 @@ export const Hero = () => {
     <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 ">
       <div className="container mx-auto px-6 ">
         <div className=" flex flex-col justify-center items-center space-y-6 md:space-y-8 text-center ">
-          <div className="flex  items-center  gap-2  ml-[300px] md:ml-[100px] text-green-500 w-[600px] font-mono md:min-h-[80px]  text-4xl md:text-6xl tracking-tighter ">
-            <div className="font-black">&gt;</div>
+          <div className="flex  items-center  gap-2  md:ml-[100px] text-green-500 ml-2  font-mono min-h-[80px] min-w-[260px] md:min-w-[600px] text-3xl md:text-6xl tracking-tighter ">
+            <div className="font-black ">&gt;</div>
             <Writer  text="Hey, I'm Jan!" />
           </div>
 
