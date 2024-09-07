f1f71d477f9d715b9e8c0db94c35bd00de6abea3
jansdhillon <imightbejan@gmail.com>
2024-09-07 16:38:08 -0600

    revert spacing on education

--- src/components/education.tsx
+++ src/components/education.tsx
@@ -8,7 +8,7 @@ export const Education = () => {
   return (
     <section className="w-full py-12 md:py-24 lg:py-32 " id="education">
       <div className="container mx-auto px-14">
-        <div className="flex flex-col items-center  justify-center space-y-12 text-center">
+        <div className="flex flex-col items-center  justify-center space-y-8 text-center">
           <div>
             <Badge className="space-x-2 " variant={"secondary"}>
               <div>📚</div> <p>Education</p>
