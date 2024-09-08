b369e1b959cc8e54a102eae870206cc51b6951aa
jansdhillon <imightbejan@gmail.com>
2024-09-07 19:57:37 -0600

    revert font change

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -23,7 +23,7 @@ const NavLink = ({ href, children, onClick = () => {} }: any) => {
   return (
     <Link
       href={href}
-      className={`text-sm font-semibold hover:text-primary  transition-colors ${
+      className={`text-sm font-medium hover:text-primary  transition-colors ${
         isActive ? "text-primary" : "text-muted-foreground"
       }`}
       onClick={onClick}
