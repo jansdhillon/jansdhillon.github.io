5536c5c75fabe7feb1d53ca4c4cc711aba122923
jansdhillon <imightbejan@gmail.com>
2024-09-07 19:25:08 -0600

    fix terminal break on mobile" "

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -10,7 +10,7 @@ export const Hero = () => {
     <section className="w-full py-8 md:py-20 lg:py-24 xl:py-36 ">
       <div className="container mx-auto px-6 ">
         <div className=" flex flex-col justify-center items-center space-y-6 md:space-y-8 text-center ">
-          <div className=" bg-card rounded-xl  border-2 border-muted-foreground/50 flex flex-col    font-mono md:min-h-[80px] w-[310px] md:w-[570px] text-3xl md:text-6xl tracking-tighter ">
+          <div className=" bg-card rounded-xl  border-2 border-muted-foreground/50 flex flex-col    font-mono md:min-h-[80px] w-[330px] md:w-[570px] text-3xl md:text-6xl tracking-tighter ">
             <div className="p-4 rounded-lg bg-secondary flex gap-3">
               <Circle className="h-3 w-3 md:h-4 md:w-4 text-red-500 fill-red-500" />
               <Circle className="h-3 w-3 md:h-4 md:w-4 text-yellow-500 fill-yellow-500" />
@@ -27,7 +27,7 @@ export const Hero = () => {
           <h2 className="text-lg md:text-2xl font-semibold   md:pb-2">
             Software Engineer
           </h2>
-          <Separator className="max-w-[310px] md:max-w-[600px]" />
+          <Separator className="max-w-[330px] md:max-w-[600px]" />
           <Socials />
         </div>
       </div>
